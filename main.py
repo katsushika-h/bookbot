f386cc92c6d7d0b2822034475fc9e0ef18f8494c
katsushika-h <chromamonster@gmail.com>
2025-02-26 00:47:33 +0800

    bookbot report

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_character_count, dict_to_list, report, Count
+
+#get_character_count(get_book_text('books/frankenstein.txt'))
+#get_num_words(get_book_text('books/frankenstein.txt'))
+
+#sort_dictionary(
+#    get_num_words(get_book_text('books/frankenstein.txt')), 
+#    get_character_count(get_book_text('books/frankenstein.txt'))
+#    )
 
 
-get_num_words(get_book_text('books/frankenstein.txt'))
+book_text = get_book_text('books/frankenstein.txt')
+num_words = get_num_words(book_text)
+character_count = get_character_count(book_text)
+#print(character_count)
+list_words = dict_to_list(character_count)
+
+report(num_words, list_words)
--- stats.py
+++ stats.py
@@ -7,3 +7,42 @@ def get_book_text(path):
 def get_num_words(text):
     split_words = text.split()
     print(str(len(split_words)) +" words found in the document")
+    return len(split_words)
+
+def get_character_count(text):
+    symbol_dict = {}
+    text = text.lower()
+
+    for character in text:
+        if not character in symbol_dict:
+            symbol_dict[character] = 1
+        else:
+            symbol_dict[character] += 1
+    return symbol_dict
+
+def Count(number):
+    return number['Count']
+
+def dict_to_list(dict):
+    list = []
+    for item in dict:
+        list.append({"Character":item, "Count":dict[item]})
+    
+    list.sort(key=Count, reverse=True)
+    #print(list)
+    return(list)
+    
+
+
+def report(word_count, character_dict):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in character_dict:
+        print(f"{item['Character']}: {item['Count']}")
+    
+    print("============= END ===============")
+
+
